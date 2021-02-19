6e728456201d776371ca5fe8acfee30b6b4a8764
Bob Zhang <chz026@ucsd.edu>
2021-02-18 21:02:39 -0800

    ignore target, starter code + rand crate for guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,16 @@
+use std::io; // io library from std lib
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please enter your guess!");
+
+    let mut guess = String::new(); // vars default immutable. mut = make it mutable
+
+    // std::io::stdin() // if the first line is not there
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
